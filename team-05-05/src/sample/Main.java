dd648aadbf84d782df2f736819f4944f4d2cab4c
Bernadette <bdavoud3@gmail.com>
2021-04-05 20:28:48 -0700

    this time change for real

--- team-05-05/src/sample/Main.java
+++ team-05-05/src/sample/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello team 5");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
