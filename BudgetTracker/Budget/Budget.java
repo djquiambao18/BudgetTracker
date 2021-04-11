7054fde202dad4ef05c7c189340dabb5202549e7
djquiambao18 <djquiambao.18@gmail.com>
2021-04-10 18:45:00 -0700

    current version 0.1

---
+++ BudgetTracker/Budget/Budget.java
@@ -0,0 +1,48 @@
+package BudgetTracker.Budget;
+
+public class Budget 
+{
+    private double remainingBudget;
+    private double budgetLimit;
+    private double budgetAmount;
+    private double budgetPct;
+    private double totalExpenses;
+
+
+    //Budget constructor starts here
+    public Budget(double budgetAmount, double budgetLimit) 
+    {
+        budgetAmount= this.budgetLimit;
+        budgetLimit = this.budgetLimit;
+    }
+    //Budget ends starts here
+
+
+    // getRemainigBudget method
+    public double getRemainingBudget()
+    {
+        remainingBudget =  budgetLimit - totalExpenses;
+        return remainingBudget;
+    }
+
+    
+    //addMoney method
+    public void addMoney(double money)
+
+    {
+       budgetLimit = budgetLimit + money;
+    }
+
+    
+    public void setBudget(double amount)
+    {
+        this.budgetLimit = amount;
+    }
+    
+    
+    public double getBudgetPct() 
+    {
+        return remainingBudget / (budgetLimit);
+    }
+
+}
